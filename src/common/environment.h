44ff141c306f3ccdbe99c4e637c93838e9af1aee
Anthony Bradford <anthony_bradford@yahoo.com>
2016-02-14 21:41:44 -0500

    Added proper header to environment.h

--- src/common/environment.h
+++ src/common/environment.h
@@ -1,3 +1,32 @@
+/* optionmatrix:                                                            */
+/*                                                                          */
+/* Options & Futures Matrix Modeler                                         */
+/* View and Control Theoretical Option Chains                               */
+/*                                                                          */
+/* File: environment.h of optionmatrix                                      */
+/*                                                                          */
+/* Copyright (c) Anthony Bradford. 2012.                                    */
+/* http://anthonybradford.com                                               */
+/* info@anthonybradford.com                                                 */
+/*                                                                          */
+/* optionmatrix may be freely redistributed.                                */
+/* See file COPYING included with this distribution for license information */
+
+/* 
+   optionmatrix is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   optionmatrix program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // Check windows
 #if _WIN32 || _WIN64
 #if _WIN64
