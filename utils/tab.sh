e42d3931741f7f8c083ee734708abb716b89e043
Anthony Bradford <anthony_bradford@yahoo.com>
2016-06-09 20:10:36 -0400

    Mentioned to use emacs untabify to remove TABs.

--- utils/tab.sh
+++ utils/tab.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Copyright (C) 2012 Anthony Bradford.
-# Check source code for TABs
+# Check source code for TABs.
+# In emacs run untabify to remove TABs.
 
 grep -P '	' ../src/gtk/*.h ../src/gtk/*.cpp
 grep -P '	' ../src/curses/*.h ../src/curses/*.cpp
