fadf5556aff5896e2e40a2773fa915f6ec0d08a0
Anthony Bradford <anthony_bradford@yahoo.com>
2020-02-09 21:02:55 -0500

    Added 'make' command or yum install.

--- utils/yum.sh
+++ utils/yum.sh
@@ -6,4 +6,4 @@ echo
 echo Install proper packages for GNU/Linux distributions using the rpm package manager
 echo
 
-yum install gcc-c++ clang ncurses-devel gtk2-devel gtk3-devel gsl gsl-devel help2man boost-devel texinfo texlive texinfo-tex
+yum install gcc-c++ clang ncurses-devel gtk2-devel gtk3-devel gsl gsl-devel help2man boost-devel texinfo texlive texinfo-tex make
