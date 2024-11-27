12bb6016748c0d34ecb0d5d8565ddec10dfc1962
Anthony Bradford <anthony_bradford@yahoo.com>
2024-11-27 01:41:25 -0500

    Scripts to test make builds.

--- make_tests.sh
+++ make_tests.sh
@@ -2,16 +2,23 @@
 
 # A. Bradford
 
+# run command "$ script" prior to running this bash script
+# when the bash script is done type Control-d
+# Analyse with
+# egrep -i 'warning| Status:' typescript
+
 # doc/Makefile.am has optionmatrix.pdf and optionmatrix.info in EXTRA_DIST
 # Texinfo should not be a required package for "make optionmatrix"
 # Don't run make distclean it deletes optionmatrix.pdf in the
 # distribution. make distclean does not delete optionmatrix.info
-#make distclean
 
 # We should be able to build optionmatrix without the following libraries
 sudo apt-get remove libnewmat10-dev
 sudo apt-get remove libitpp-dev
 sudo apt-get remove libboost-all-dev
+
+# Need GTK+3 and curses
+sudo apt-get install libgtk-3-dev libncurses-dev
 
 # QuantLib does not exist as a apt-get un-installable package.
 # Do the following on actual QuantLib autotools package
@@ -22,70 +29,67 @@ sudo apt-get remove libboost-all-dev
 # see EXTRA_DIST in doc/Makefile.am
 sudo apt-get remove texinfo
 
-./configure
-make
+make clean; ./configure CXX=g++; make
+make check
 ./src/gtk/optionmatrix --list
-echo The number of option models should be less since we removed libraries
-read -n 1 -s -r -p "[1] Press any key to continue"
+# 167 option model count does not include models named like "Testing Option"  or "Testing Future"
+echo "[1] Status: The number of option models should be 167 since we removed libraries"
+echo "[1] Status: Built with g++/GTK+3"
+echo "[1] Status: Built without libnewmat10-dev libitpp-dev libboost-all-dev texinfo"
+echo "[1] Status: The build should *not* have dependency on texinfo"
 echo
 
 # We should be able to build optionmatrix with the following libraries and
-# texinfo. The inclusion of libnewmat10-dev, libitpp-dev and
-# libboost-all-dev add 3 more models.
+# The inclusion of libnewmat10-dev, libitpp-dev and libboost-all-dev add
+# 3 more models.
 sudo apt-get install libnewmat10-dev
 sudo apt-get install libitpp-dev
 sudo apt-get install libboost-all-dev
 sudo apt-get install texinfo
 
-# QuantLib does not exist as a apt-get installable package.
-# Do the following on actual QuantLib autotools package
-# cd QuantLib; sudo make ; sudo make install
-
-./configure
-make
+make clean; ./configure CXX=g++; make
+make check
 ./src/gtk/optionmatrix --list
-echo The number of option models should be more since we added libraries
-read -n 1 -s -r -p "[2] Press any key to continue"
+# 170 option model count does not include models named like "Testing Option"  or "Testing Future"
+echo "[2] Status: The number of option models should be 170 more since we added libraries"
+echo "[2] Status: Built with g++/GTK+3 libnewmat10-dev libitpp-dev libboost-all-dev texinfo"
 echo
 
-# Check for compiler warnings/errors with clang
+# Build with clang++
 make clean; ./configure CXX=clang++ ; make
 make check
 ./src/gtk/optionmatrix --list
-echo Built with clang
-read -n 1 -s -r -p "[3] Press any key to continue"
+echo "[3] Status: Built with clang/GTK+3"
 echo
 
-# Check for compiler warnings/errors with g++
-make clean ./configure CXX=g++ ; make
-make check
-./src/gtk/optionmatrix --list
-echo Built again with g++
-read -n 1 -s -r -p "[4] Press any key to continue"
-echo
-
-# Remove GTK+ 3 and compile with GTK+ 2 (apt-get for Ubuntu)
+# Remove GTK+3 and build with GTK+ 2 (apt-get for Ubuntu)
 sudo apt-get remove libgtk-3-dev
 sudo apt-get install libgtk2.0-dev
-make clean; ./configure ; make
-make check
-echo Built with GTK+ 2
-read -n 1 -s -r -p "[5] Press any key to continue"
 
-# Reinstall GTK+ 3 (apt-get for Ubuntu)
-sudo apt-get install libgtk-3-dev
-make clean; ./configure ; make
+make clean; ./configure CXX=g++; make
 make check
-echo Built with GTK+ 3
-read -n 1 -s -r -p "[6] Press any key to continue"
+echo "[5] Status: Built with g++/GTK+2"
+
+# Build with clang++
+make clean; ./configure CXX=clang++ ; make
+make check
+./src/gtk/optionmatrix --list
+echo "[6] Status: Built with clang/GTK+2"
+echo
+
+# Reinstall GTK+3 (apt-get for Ubuntu)
+sudo apt-get install libgtk-3-dev
+
+make clean; ./configure CXX=g++; make
+make check
+echo "[7] Status: Built with g++/GTK+3"
 
 sudo make install
+echo "[8] Status: These are the installed optionmatrix files"
 # The following should yield installed optionmatrix files
 find /usr -name "*optionmatrix*" -print 2>/dev/null
-echo These are the installed optionmatrix files
-read -n 1 -s -r -p "[7] Press any key to continue"
 
 sudo make uninstall
+# The following should yield *no* files since we uninstalled optionmatrix
 find /usr -name "*optionmatrix*" -print 2>/dev/null
-echo optionmatrix uninstalled. No optionmatrix files should be found in /usr
-read -n 1 -s -r -p "[8] Press any key to continue"
+echo "[9] Status: optionmatrix uninstalled. No optionmatrix files should be found in /usr"
---
+++ make_tests_quantlib.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# A. Bradford
+
+# Testing building OptionMatrix with and without QuantLib
+
+# run command "$ script" prior to running this bash script
+# when the bash script is done type Control-d
+# Analyse with
+# egrep -i 'warning| Status:' typescript
+
+# QuantLib does not exist as an apt-get installable package
+# Do the following on actual QuantLib autotools package
+# On my machine I have QuantLib located in ~/src/c++/QuantLib-1.36/
+# Your location will differ
+# The next line will uninstall QuantLib
+(cd ~/src/c++/QuantLib-1.36/ && sudo make uninstall)
+
+# Build optionmatrix without QuantLib
+make clean; ./configure CXX=g++ ; make
+make check
+./src/gtk/optionmatrix --list
+echo "[1] Status: Built with g++"
+echo "[1] Status: Built without QuantLib"
+echo
+
+# QuantLib requires Boost
+sudo apt-get install libboost-all-dev
+
+# QuantLib does not exist as an apt-get installable package
+# Do the following on actual QuantLib autotools package
+# On my machine I have QuantLib located in ~/src/c++/QuantLib-1.36/
+# Your location will differ
+# The next line will install QuantLib
+(cd ~/src/c++/QuantLib-1.36/ && make && sudo make install)
+
+# Build optionmatrix with QuantLib
+make clean; ./configure CXX=g++ ; make
+make check
+./src/gtk/optionmatrix --list
+echo "[2] Status: Built with g++/QuantLib"
+echo "[2] Status: QuantLib addes 29 more option models"
+echo
