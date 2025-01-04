a1e679bdb3873f57881fd52a44618f15df8a4307
Anthony Bradford <anthony_bradford@yahoo.com>
2025-01-04 18:04:06 -0500

    Mostly changes in echo and comments.

--- make_tests.sh
+++ make_tests.sh
@@ -2,27 +2,36 @@
 
 # A. Bradford
 
+# This script assumes your machine is using the apt-get package manager
+
 # run command "$ script" prior to running this bash script
 # when the bash script is done type Control-d
 # Analyse with
-# egrep -i 'warning| Status:' typescript
+# grep -E -i 'warning| Status:' typescript
 
 # doc/Makefile.am has optionmatrix.pdf and optionmatrix.info in EXTRA_DIST
 # Texinfo should not be a required package for "make optionmatrix"
 # Don't run make distclean it deletes optionmatrix.pdf in the
 # distribution. make distclean does not delete optionmatrix.info
 
+# We should be able to build optionmatrix with GTK+3 (or GTK+2) and without libncurses-dev
+sudo apt-get remove libncurses-dev
+sudo apt-get install libgtk-3-dev
+
+make clean; ./configure CXX=g++; make
+make check
+./src/gtk/optionmatrix --version
+echo "[1] Status: Built with g++/GTK+3"
+echo "[1] Status: Built without libncurses-dev"
+echo
+
 # We should be able to build optionmatrix without the following libraries
 sudo apt-get remove libnewmat10-dev
 sudo apt-get remove libitpp-dev
 sudo apt-get remove libboost-all-dev
 
-# Need GTK+3 and curses
-sudo apt-get install libgtk-3-dev libncurses-dev
-
-# QuantLib does not exist as a apt-get un-installable package.
-# Do the following on actual QuantLib autotools package
-# cd QuantLib; sudo make uninstall
+# Add curses to generate src/curses/optionmatrix
+sudo apt-get libncurses-dev
 
 # optionmatrix.info and optionmatrix.pdf require texinfo to be built, but
 # they do not have to be build since they are included in the distribution
@@ -31,15 +40,18 @@ sudo apt-get remove texinfo
 
 make clean; ./configure CXX=g++; make
 make check
+# Curses version should succesfully build
+./src/curses/optionmatrix --version
+# Check model count
 ./src/gtk/optionmatrix --list
 # 167 option model count does not include models named like "Testing Option"  or "Testing Future"
-echo "[1] Status: The number of option models should be 167 since we removed libraries"
-echo "[1] Status: Built with g++/GTK+3"
-echo "[1] Status: Built without libnewmat10-dev libitpp-dev libboost-all-dev texinfo"
-echo "[1] Status: The build should *not* have dependency on texinfo"
+echo "[2] Status: Number of option models should be 167"
+echo "[2] Status: Built with g++/GTK+3"
+echo "[2] Status: Built without libaries libnewmat10-dev libitpp-dev libboost-all-dev texinfo"
+echo "[2] Status: The build should *not* have dependency on texinfo"
 echo
 
-# We should be able to build optionmatrix with the following libraries and
+# We should be able to build optionmatrix with the following libraries.
 # The inclusion of libnewmat10-dev, libitpp-dev and libboost-all-dev add
 # 3 more models.
 sudo apt-get install libnewmat10-dev
@@ -51,15 +63,15 @@ make clean; ./configure CXX=g++; make
 make check
 ./src/gtk/optionmatrix --list
 # 170 option model count does not include models named like "Testing Option"  or "Testing Future"
-echo "[2] Status: The number of option models should be 170 more since we added libraries"
-echo "[2] Status: Built with g++/GTK+3 libnewmat10-dev libitpp-dev libboost-all-dev texinfo"
+echo "[3] Status: The number of option models should be 170 more since we added libraries"
+echo "[3] Status: Built with g++/GTK+3 libnewmat10-dev libitpp-dev libboost-all-dev texinfo"
 echo
 
 # Build with clang++
 make clean; ./configure CXX=clang++ ; make
 make check
 ./src/gtk/optionmatrix --list
-echo "[3] Status: Built with clang/GTK+3"
+echo "[4] Status: Built with clang/GTK+3"
 echo
 
 # Remove GTK+3 and build with GTK+ 2 (apt-get for Ubuntu)
